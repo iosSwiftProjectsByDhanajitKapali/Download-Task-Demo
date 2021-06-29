8e66d60ec30a629f9d7bca8f9e7ec6b0a7b0c6c7
Dhanajit kapali <37000235+dhanajitkapali@users.noreply.github.com>
2021-06-29 16:56:19 +0530

    added function to download large files, added progress bar to show its progress, added checks to disable download button during download

---
+++ Download Task Demo/Views/HomeViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  Download Task Demo
+//
+//  Created by unthinkable-mac-0025 on 29/06/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet var downloadButton: UIButton!
+    @IBOutlet var progressBar: UIProgressView!
+    @IBOutlet var acitvityIndicator: UIActivityIndicatorView!
+    
+    private var presenter : HomePresenter!
+    
+    private var documentController : UIDocumentInteractionController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        presenter = HomePresenter(withDelegate: self)
+        documentController = UIDocumentInteractionController()
+        
+        progressBar.progress = 0
+        acitvityIndicator.isHidden = true
+    }
+
+    @IBAction func downloadButtonPressed(_ sender: UIButton) {
+        presenter.getFile()
+        downloadButton.isEnabled = false
+        acitvityIndicator.isHidden = false
+        acitvityIndicator.startAnimating()
+    }
+    
+
+}
+
+extension HomeViewController : HomePresenterDelegate{
+    func updateProgressBar(withProgress: Float) {
+        DispatchQueue.main.async { [self] in
+            progressBar.progress = withProgress
+        }
+    }
+    
+    func presentAlert(title: String, message: String) {
+        DispatchQueue.main.async { [self] in
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Alert", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+        }
+    }
+    
+    func previewAndShareFile(atLocation: URL) {
+        DispatchQueue.main.async { [self] in
+            acitvityIndicator.stopAnimating()
+            acitvityIndicator.isHidden = true
+            downloadButton.isEnabled = true
+            
+            //initiate the shareSheet with its preview
+            documentController.url = atLocation
+            documentController.delegate = self
+            documentController.presentPreview(animated: true)
+        }
+    }
+    
+}
+
+extension HomeViewController : UIDocumentInteractionControllerDelegate{
+    func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
+        return self
+    }
+    
+    func documentInteractionControllerDidEndPreview(_ controller: UIDocumentInteractionController) {
+        progressBar.progress = 0
+    }
+    
+    
+}
